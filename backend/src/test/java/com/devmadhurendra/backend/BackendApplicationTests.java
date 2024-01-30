a93f3a08d7ed4dae751398d58f5ed231786f2d20
dev-madhurendra <madhuredra.tiwari@zemosolabs.com>
2024-01-30 13:29:36 +0530

    📁 feat : added root foler

---
+++ backend/src/test/java/com/devmadhurendra/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmadhurendra.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
