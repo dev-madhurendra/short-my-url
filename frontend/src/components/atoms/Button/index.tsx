caa164935256d680cd70e409c03d237e1aff9517
dev-madhurendra <madhuredra.tiwari@zemosolabs.com>
2024-01-31 13:15:31 +0530

    :test_tube: feat : testing CI/CD fe

--- frontend/src/components/atoms/Button/index.tsx
+++ frontend/src/components/atoms/Button/index.tsx
@@ -2,8 +2,6 @@ import { Button, ButtonProps } from "@mui/material";
 
 interface ButtonPropsInterface extends ButtonProps {}
 
-const MuiButton = ({ ...buttonProps }: ButtonPropsInterface) => {
-  return <Button {...buttonProps} disableElevation disableRipple />;
-};
+const MuiButton = ({ ...buttonProps }: ButtonPropsInterface) => <Button {...buttonProps} disableElevation disableRipple />;
 
 export default MuiButton;
